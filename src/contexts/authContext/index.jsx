263c90951ee057a14e8e5025cb67b697f43485fb
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-07-02 05:15:54 +0530

    firebase

---
+++ src/contexts/authContext/index.jsx
@@ -0,0 +1,13 @@
+import { auth } from "../../firebase/firebase";
+
+const AuthContext = React.createContext();
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, initializeUser);
+  }, []);
+}
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
+import { getAuth } from "firebase/auth";
 //Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBCpEyzPGBeiVC23prGdjznQZ6DpkOqZdI",
@@ -14,4 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export { app, auth };
